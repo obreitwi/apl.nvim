4888d636a7a2d5c4ffefd4e7ea3450d551230b60
Oliver Breitwieser <oliver@breitwieser.eu>
2025-06-20 23:17:26 +0200

    fix: adapt health checks

--- lua/apl/health.lua
+++ lua/apl/health.lua
@@ -3,7 +3,6 @@
 ---@usage :checkhealth apl
 ---@local
 
-local health = require 'health'
 -- local install = require 'apl.install'
 local lang = require 'apl.lang'
 local config = require 'apl.config'
@@ -14,40 +13,40 @@ local M = {}
 local function check_nvim_version()
   local supported = vim.fn.has 'nvim-0.7' == 1
   if not supported then
-    health.report_error 'apl.nvim needs nvim version 0.7 or higher.'
-    health.report_info 'if you are unable to upgrade, use the `0.6-compat` branch'
+    vim.health.error 'apl.nvim needs nvim version 0.7 or higher.'
+    vim.health.info 'if you are unable to upgrade, use the `0.6-compat` branch'
   else
     local v = vim.version()
-    health.report_ok(string.format('nvim version %d.%d.%d', v.major, v.minor, v.patch))
+    vim.health.ok(string.format('nvim version %d.%d.%d', v.major, v.minor, v.patch))
   end
 end
 
 -- local function check_classes_installed()
 --   local class_path = install.check()
 --   if not class_path then
---     health.report_error 'apl classes are not installed.'
---     health.report_info 'use `ensure_installed = true` in the apl setup function'
+--     vim.health.error 'apl classes are not installed.'
+--     vim.health.info 'use `ensure_installed = true` in the apl setup function'
 --   else
---     health.report_ok('apl classes are installed: ' .. class_path)
+--     vim.health.ok('apl classes are installed: ' .. class_path)
 --   end
 -- end
 
 local function check_keymaps()
   if vim.tbl_count(config.keymaps) == 0 then
-    health.report_info 'no keymaps defined'
+    vim.health.info 'no keymaps defined'
   else
-    health.report_ok 'keymaps are defined'
+    vim.health.ok 'keymaps are defined'
   end
 end
 
 -- local function check_documentation()
 --   local doc = config.documentation
 --   if not doc.cmd then
---     health.report_info 'using HelpBrowser for documentation'
+--     vim.health.info 'using HelpBrowser for documentation'
 --   else
 --     local exe_path = vim.fn.exepath(doc.cmd)
 --     if exe_path ~= '' then
---       health.report_ok(doc.cmd)
+--       vim.health.ok(doc.cmd)
 --     end
 --     if doc.args then
 --       local vin = false
@@ -60,20 +59,20 @@ end
 --         end
 --       end
 --       if vin and vout then
---         health.report_ok(vim.inspect(doc.args))
+--         vim.health.ok(vim.inspect(doc.args))
 --       elseif vout and not vin then
---         health.report_error 'argument list is missing input placeholder ($1)'
+--         vim.health.error 'argument list is missing input placeholder ($1)'
 --       elseif vin and not vout then
---         health.report_error 'argument list is missing output placeholder ($2)'
+--         vim.health.error 'argument list is missing output placeholder ($2)'
 --       else
---         health.report_error 'argument list is missing both input and output placeholders ($1/$2)'
+--         vim.health.error 'argument list is missing both input and output placeholders ($1/$2)'
 --       end
 --     end
 --     if doc.on_open then
---       health.report_info 'using external function for on_open'
+--       vim.health.info 'using external function for on_open'
 --     end
 --     if doc.on_select then
---       health.report_info 'using external function for on_select'
+--       vim.health.info 'using external function for on_select'
 --     end
 --   end
 -- end
@@ -81,9 +80,9 @@ end
 -- local function check_lang()
 --   local ok, ret = pcall(lang.find_lang_executable)
 --   if ok then
---     health.report_ok('lang executable: ' .. ret)
+--     vim.health.ok('lang executable: ' .. ret)
 --   else
---     health.report_error(ret)
+--     vim.health.error(ret)
 --   end
 -- end
 
@@ -96,29 +95,29 @@ local function check_extensions()
   for _, name in ipairs(installed) do
     local health_check = extensions._health[name]
     if health_check then
-      health.report_start(string.format('extension: "%s"', name))
+      vim.health.start(string.format('extension: "%s"', name))
       health_check()
       local link = extensions._linked[name]
       if link then
-        health.report_ok(string.format('installed classes "%s"', link))
+        vim.health.ok(string.format('installed classes "%s"', link))
       else
-        health.report_ok 'no classes to install'
+        vim.health.ok 'no classes to install'
       end
     else
-      health.report_ok(string.format('No health check for "%s"', name))
+      vim.health.ok(string.format('No vim.health check for "%s"', name))
     end
   end
 end
 
 function M.check()
-  health.report_start 'apl'
+  vim.health.start 'apl'
   check_nvim_version()
   -- check_lang()
   -- check_classes_installed()
   check_keymaps()
-  -- health.report_start 'apl documentation'
+  -- vim.health.start 'apl documentation'
   -- check_documentation()
-  health.report_start 'apl extensions'
+  vim.health.start 'apl extensions'
   check_extensions()
 end
 
